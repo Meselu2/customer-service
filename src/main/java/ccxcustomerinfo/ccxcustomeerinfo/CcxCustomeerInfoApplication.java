0b8bb49f453f62de90d33b989a0cffdd56611d8a
meselu <meselu@meselus-Air.lan>
2023-08-08 16:42:19 -0500

    project-setup

---
+++ src/main/java/ccxcustomerinfo/ccxcustomeerinfo/CcxCustomeerInfoApplication.java
@@ -0,0 +1,13 @@
+package ccxcustomerinfo.ccxcustomeerinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcxCustomeerInfoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcxCustomeerInfoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ccxcustomerinfo/ccxcustomeerinfo/CcxCustomeerInfoApplicationTests.java
@@ -0,0 +1,13 @@
+package ccxcustomerinfo.ccxcustomeerinfo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcxCustomeerInfoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
